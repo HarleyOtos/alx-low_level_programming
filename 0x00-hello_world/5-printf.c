0ca337331e7fd6121b37dbd4eb1c8f834e3ec6ba
Harley O <harleyotos@gmail.com>
2022-09-08 20:52:02 -0700

    Awesome C task commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print function
+ *
+ * Return: Always 0 (success message)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
