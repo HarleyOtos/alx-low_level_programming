1d99d06b9ca813694debb8a699ade002deb634f7
Harley O <harleyotos@gmail.com>
2022-09-14 14:54:59 -0700

    commit of task 9 - 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two integers.
+ * @num1: First integer
+ * @num2: Second integer
+ *
+ * Return: Always 0
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
