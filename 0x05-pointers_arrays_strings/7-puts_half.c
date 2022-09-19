9209569699baf2b79438866efa543b3be2b74bac
Harley O <harleyotos@gmail.com>
2022-09-19 15:11:33 -0700

    7-puts_half updated

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,16 +8,16 @@
 
 void puts_half(char *str)
 {
-	int i = 0, len = 0, n;
+	int index = 0, len = 0, n;
 
-	while (str[i++])
+	while (str[index++])
 		len++;
 
 	if ((len % 2) == 0)
 		n = len / 2;
 	else
 		n = (len + 1) / 2;
-	for (i = n; i < len; i++)
-		_putchar(str[i]);
+	for (index = n; index < len; index++)
+		_putchar(str[index]);
 	_putchar('\n');
 }
