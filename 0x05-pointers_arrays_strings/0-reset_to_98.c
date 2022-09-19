0f99d8190dd302fa3257063d77726dfe0e3c35ee
Harley O <harleyotos@gmail.com>
2022-09-19 09:37:22 -0700

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -a function that reset and updates the value it points to 98
+ * @n: the used parameter
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
