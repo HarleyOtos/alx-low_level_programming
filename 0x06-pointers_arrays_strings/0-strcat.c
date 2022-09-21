48e722c3250136038d465fb479d2c76e4fe111f1
Harley O <harleyotos@gmail.com>
2022-09-21 12:54:04 -0700

    0-strcat task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,30 @@
 #include "main.h"
 
 /**
- * strcat - a function that concatenates two strings
+ * _strcat - a function that concatenates two strings
  * @dest: The pointer
  * @src: the source parameter
+ * @n: The amount of bytes
  *
  * Return: dest
  */
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int count = 0, count2 = 0;
 
-	while (dest[index++])
-		dest_len++;
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
 
 	return (dest);
 }
