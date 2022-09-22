436ab6b7008df1be9c2bdcefbc127f8d644c9042
Harley <111179132+HarleyOtos@users.noreply.github.com>
2022-09-22 19:59:40 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,28 +4,33 @@
  * cap_string - a function that changes all lowercase
  * letters of a string to uppercase
  * @s: The input string
- * Return: dest
+ * Return: A changed string
  */
 char *cap_string(char *s)
 {
-	int count = 0, i;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
+	int i = 0;
+	
+	while (s[i])
 	{
-		for (i = 0; i < 13; i++)
-		{
-			if (*(s + count) == sep_words[i])
-			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-				break;
-			}
-		}
-		count++;
+		while (!(s[i] >= 'a' && s[i] <= 'z'))
+			i++;
+		if (s[i - 1] == ' ' ||
+		    s[i - 1] == '\t' ||
+		     s[i - 1] == '\n' ||
+		      s[i - 1] == ',' ||
+		       s[i - 1] == ';' ||
+			s[i - 1] == '.' ||
+			 s[i - 1] == '!' ||
+			  s[i - 1] == '?' ||
+			   s[i - 1] == '"' ||
+			    s[i - 1] == '(' ||
+			     s[i - 1] == ')' ||
+			      s[i - 1] == '{' ||
+			       s[i - 1] == '}' ||
+				i == 0)
+			s[i] -= 32;
+		
+		i++;
 	}
 	return (s);
 }
