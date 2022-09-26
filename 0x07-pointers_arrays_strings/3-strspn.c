2ad19df6f61a8250765a28bf1830e67ec36c1dab
Harley <111179132+HarleyOtos@users.noreply.github.com>
2022-09-26 19:45:13 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,7 +9,6 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-<<<<<<< HEAD
 	unsigned int bytes = 0;
 	int i;
 
@@ -28,24 +27,4 @@ unsigned int _strspn(char *s, char *accept)
 		s++;
 	}
 	return (bytes);
-=======
-    unsigned int bytes = 0;
-    int i;
-    
-    while (*s)
-    {
-        for (i = 0; accept[i]; i++)
-        {
-            if (*s == accept[i])
-            {
-                bytes++;
-                break;
-            }
-            else if (accept[i + 1] == '\0')
-                return (bytes);
-        }
-        s++;
-    }
-    return (bytes);
->>>>>>> 53eb7f05bd6f5ec7ad437734a918e890df2b3906
 }
