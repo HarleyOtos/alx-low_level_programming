53eb7f05bd6f5ec7ad437734a918e890df2b3906
Harley <111179132+HarleyOtos@users.noreply.github.com>
2022-09-26 19:09:15 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,7 +12,7 @@ unsigned int _strspn(char *s, char *accept)
 {
     unsigned int bytes = 0;
     int i;
-
+    
     while (*s)
     {
         for (i = 0; accept[i]; i++)
@@ -27,6 +27,5 @@ unsigned int _strspn(char *s, char *accept)
         }
         s++;
     }
-
     return (bytes);
 }
