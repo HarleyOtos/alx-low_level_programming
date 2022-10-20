32785681d679bd93080c62c5f7b7bfef72ef3b39
Harley <111179132+HarleyOtos@users.noreply.github.com>
2022-10-20 12:13:15 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,18 +10,21 @@
   */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	int i = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
+			h = h->next;
+		}
 		else
+		{
 			printf("[%d] %s\n", h->len, h->str);
-
-		h = h->next;
-		count++;
+			h = h->next;
+		}
+		i++;
 	}
-
-	return (count);
+	return (i);
 }
