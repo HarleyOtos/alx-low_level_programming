995d0b48017ccebf40bccb932d1a232f24c89d80
Harley O <harleyotos@gmail.com>
2022-10-25 00:46:31 -0700

    0-print_listint.c fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,12 +2,13 @@
 #include "lists.h"
 
 /**
-  * print_listint - A function to prints all the
-  * elements of a linked list
-  * @h: The head of the linked list
-  *
-  * Return: The number of nodes of the linked list
-  */
+ * print_listint - A function to prints all the
+ * elements of a linked list
+ * @h: The head of the linked list
+ *
+ * Return: The number of nodes of the linked list
+ */
+
 size_t print_listint(const listint_t *h)
 {
 	int nodes = 0;
