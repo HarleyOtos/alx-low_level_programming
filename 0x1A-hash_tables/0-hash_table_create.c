e56373048f98b8bb657c945f929b54de08a14929
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-01-06 20:25:11 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,23 +8,23 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i = 0;
-	hash_table_t *htable = NULL;
-	hash_node_t **arr = NULL;
+	hash_table_t *htable;
+
+	if (size < 1)
+		return (NULL);
 
 	htable = malloc(sizeof(hash_table_t));
 	if (htable == NULL)
 		return (NULL);
 
-	arr = malloc(sizeof(hash_node_t *) * size);
-	if (arr == NULL)
-		return (NULL);
-
-	for (; i < size; ++i)
-		arr[i] = NULL;
-
+	htable->array = calloc(size, sizeof(hash_node_t *));
 	htable->size = size;
-	htable->array = arr;
+
+	if ((htable->array) == NULL)
+	{
+		free(htable);
+		return (NULL);
+	}
 
 	return (htable);
 }
