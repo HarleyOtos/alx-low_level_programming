f1bf28e72ef0621932035b2cbb5bd444081be927
Harley O <harleyotos@gmail.com>
2023-04-11 16:01:09 -0700

    chmod

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -13,38 +13,43 @@
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-    size_t jump = sqrt(size);
-    listint_t *prev = NULL;
-    listint_t *curr = list;
+	size_t jump = sqrt(size);
+	listint_t *prev = NULL;
+	listint_t *curr = list;
 
-    if (list == NULL)
-        return (NULL);
+	if (list == NULL)
+		return (NULL);
 
-    while (curr->n < value && curr->next != NULL)
-    {
-        prev = curr;
-        for (size_t i = 0; i < jump && curr->next != NULL; i++)
-            curr = curr->next;
-        printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
-    }
+	while (curr->n < value && curr->next != NULL)
+	{
+		prev = curr;
+		for (size_t i = 0; i < jump && curr->next != NULL; i++)
+			curr = curr->next;
+		printf("Value checked at index [%lu] = [%d]\n",
+				curr->index, curr->n);
+	}
 
-    printf("Value found between indexes [%lu] and [%lu]\n", prev->index, curr->index);
+	printf("Value found between indexes [%lu] and [%lu]\n",
+			prev->index, curr->index);
 
-    size_t end = curr->index;
-    curr = prev;
-    while (curr->n < value)
-    {
-        if (curr->index == end)
-            return (NULL);
-        curr = curr->next;
-        printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
-    }
+	size_t end = curr->index;
+	curr = prev;
 
-    if (curr->n == value)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
-        return (curr);
-    }
+	while (curr->n < value)
+	{
+		if (curr->index == end)
+			return (NULL);
+		curr = curr->next;
+		printf("Value checked at index [%lu] = [%d]\n",
+				curr->index, curr->n);
+	}
 
-    return (NULL);
+	if (curr->n == value)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+				curr->index, curr->n);
+		return (curr);
+	}
+
+	return (NULL);
 }
