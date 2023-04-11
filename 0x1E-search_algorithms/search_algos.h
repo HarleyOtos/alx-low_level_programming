1ef13e0c2fe2f65adbb20250d2a03c61257c27f0
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-04-11 23:41:41 +0100

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,9 +1,29 @@
-#ifndef _BINARY_SEARCH_
-#define _BINARY_SEARCH_
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
+/* Function prototypes */
+listint_t *jump_list(listint_t *list, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
