b165f70c12dce0fd2da6ba12aa2b4c3cb346adb2
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-04-12 19:42:16 +0100

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -28,7 +28,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t low, size_t high, int value);
 int advanced_binary(int *array, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
 
